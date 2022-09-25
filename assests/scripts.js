542e8107cbc31dd0ba5ed431ec9c087b1678e837
Dong Nguyen <dnguye621@gmail.com>
2022-09-24 20:10:17 -0500

    added storage and more time and moblie friendly

--- assests/scripts.js
+++ assests/scripts.js
@@ -5,23 +5,64 @@ setInterval(function (){
 },1000)
 
 //store input information
+var schedule = {
+  hour9: "",
+  hour10: "",
+  hour11: "",
+  hour12: "",
+  hour13: "",
+  hour14: "",
+  hour15: "",
+  hour16: "",
+  hour17: "",
+}
+
+
 
 //test changing of colors
 function setStart(){
-  for (var i=0; i<2; i++){
-  var timeRow = document.body.children[1].children[i]
-  var timeData = timeRow.children[0].dataset.time;
-  var textArea = timeRow.children [1];
-  var currentHour = moment().format ("H")
+  for (var i=0; i<13; i++){
+      var timeRow = document.body.children[1].children[i]
+      var timeData = timeRow.children[0].dataset.time;
+      var textArea = timeRow.children [1];
+      var currentHour = moment().format ("H")
 
 
-  if (timeData == currentHour){
-    textArea.classList.add ("present");
-  } else if (timeData > currentHour){
-    textArea.classList.add ("future");
-  } else {
-    textArea.classList.add("past");
-  }
+      if (timeData == currentHour){
+        textArea.classList.add ("present");
+      } else if (timeData > currentHour){
+        textArea.classList.add ("future");
+      } else {
+        textArea.classList.add("past");
+      };
+      //pull from storage
+      var storedSchedule = JSON.parse(localStorage.getItem("mySchedule"));
+        if(storedSchedule !== null) {     
+          
+            var scheduleValues = Object.values(storedSchedule);
+            textArea.textContent = scheduleValues[i];
+    }
 }
 }
+
+
 setStart()
+
+//save button
+var allButtons = $("button")
+allButtons.on("click",  function(event){
+    event.preventDefault();
+    //update schedule 
+    if(JSON.parse(localStorage.getItem("mySchedule") !== null) ){
+        schedule = JSON.parse(localStorage.getItem("mySchedule"));
+    }
+
+    var eventData = $(event.target).attr("data-FormNum");
+
+    var textAreaEl = $("textarea[data-formNum='" + eventData +"'")
+    //set data value = the correct time 
+    schedule["hour" + eventData] = textAreaEl.val();
+  
+    localStorage.setItem("mySchedule", JSON.stringify(schedule));
+
+})
